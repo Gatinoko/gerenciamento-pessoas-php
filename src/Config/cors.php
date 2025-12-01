558354bb11fe2fbc639d1dc0206496d284679fe3
Gabriel Tinoco <gabrielslai@gmail.com>
2025-12-01 01:12:55 -0300

    Removed comments

--- src/Config/cors.php
+++ src/Config/cors.php
@@ -2,19 +2,11 @@
 
 namespace App\Config;
 
-// Define o cabeçalho para retornar dados em JSON
 header('Content-Type: application/json');
-
-// Permite requisições de qualquer origem (CORS - Básico para testes)
 header("Access-Control-Allow-Origin: *");
-
-// Permite os métodos HTTP que serão usados (GET, POST, PUT, DELETE)
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
-// Permite os cabeçalhos que serão enviados pelo cliente
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// Lida com o "preflight request" do CORS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
